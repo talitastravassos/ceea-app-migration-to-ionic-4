6eb0115715cc780161ddbf13ee999911f938e8fd
talitast <talitatravassos.tt@gmail.com>
2019-02-06 16:59:12 -0300

    structure of pages and services created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
+  { path: 'game', loadChildren: './pages/game/game.module#GamePageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
+  { path: 'score', loadChildren: './pages/score/score.module#ScorePageModule' },
+  { path: 'reset-password', loadChildren: './pages/reset-password/reset-password.module#ResetPasswordPageModule' },
+  { path: 'answer', loadChildren: './pages/answer/answer.module#AnswerPageModule' },
+  { path: 'result', loadChildren: './pages/result/result.module#ResultPageModule' },
+  { path: 'training', loadChildren: './pages/training/training.module#TrainingPageModule' },
 ];
 
 @NgModule({
