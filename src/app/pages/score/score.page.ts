37521dadb7e200c7a50936fc74b7964f30c23fa0
talitast <talitatravassos.tt@gmail.com>
2019-02-10 14:19:17 -0300

    Score page working and Splashscreen and icon added

--- src/app/pages/score/score.page.ts
+++ src/app/pages/score/score.page.ts
@@ -1,4 +1,6 @@
+import { ScoresService } from './../../services/scores/scores.service';
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-score',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScorePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public navCtrl: NavController,
+    public scoreService: ScoresService
+  ) { }
+
+  hits: Array<number>
+  mistakes: Array<number>
+  profit: Array<number>
+  count: number
+
+  currentScores: Array<Object>
+
+    /**
+   * getScores
+   */
+  public getScores(n: number): Object {
+
+    let scores = this.scoreService.getScore(n);
+    console.log("Pontuacao recebida: ", scores)
+
+    return scores
+  }
+
+    /*
+   * getArrayPontuacoes: função que retorna um array com todas as pontuações salvas no localStorage até
+   * o momento
+   */
+  public getArrayScores(): Array<Object> {
+    let arrayScores = []
+
+    for (let index = 1; index <= this.count; index++) {
+      let obj = this.scoreService.getScore(index);
+      arrayScores.push(obj)
+    }
+
+    //console.log("Array de objetos recebido: ", arrayPontuacoes)
+
+    return arrayScores
+  }
 
   ngOnInit() {
+
+    console.log('ScorePage');
+    this.count = this.scoreService.getCount();
+
+    this.currentScores = this.getArrayScores()
+    console.log("Pontuações Atuais: ", this.currentScores)
   }
 
 }
